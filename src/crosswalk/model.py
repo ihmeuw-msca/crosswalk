e1ca20c72088092855f63da1df8b5eed6fc2fa65
zhengp0 <zhengp@uw.edu>
2020-01-08 09:52:24 -0800

    update: add the order prior and optimizer in the model

--- src/crosswalk/model.py
+++ src/crosswalk/model.py
@@ -6,6 +6,7 @@
     `model` module of the `crosswalk` package.
 """
 import numpy as np
+from limetr import LimeTr
 from . import data
 from . import utils
 
@@ -17,8 +18,7 @@ class CWModel:
                  obs_type='diff_log',
                  cov_names=None,
                  gold_def=None,
-                 order_prior=None,
-                 direct_prior=None):
+                 order_prior=None):
         """Constructor of CWModel.
 
         Args:
@@ -33,9 +33,6 @@ class CWModel:
                 Gold standard definition.
             order_prior (list{list{str}} | None, optional):
                 Order priors between different definitions.
-            direct_prior (None, optional):
-                Direct priors on the covariates multipliers and shared between
-                definitions.
         """
         self.cwdata = cwdata
         self.obs_type = obs_type
@@ -52,7 +49,6 @@ class CWModel:
             self.gold_def = gold_def
 
         self.order_prior = order_prior
-        self.direct_prior = direct_prior
 
         self.check()
 
@@ -65,9 +61,18 @@ class CWModel:
             self.cwdata.unique_defs[i]: indices[i]
             for i in range(self.cwdata.num_defs)
         }
+        self.def_indices = {
+            self.cwdata.unique_defs[i]: i
+            for i in range(self.cwdata.num_defs)
+        }
 
-        # create the design matrix
+        # create the design and constraint matrix
         self.design_mat = self.create_design_mat()
+        self.constraint_mat = self.create_constraint_mat()
+
+        # place holder for the result
+        self.beta = None
+        self.gamma = None
 
     def check(self):
         """Check the input type, dimension and values.
@@ -87,15 +92,11 @@ class CWModel:
                 Returns relation matrix with 1 encode alternative definition
                 and -1 encode reference definition.
         """
-        indices = {
-            self.cwdata.unique_defs[i]: i
-            for i in range(self.cwdata.num_defs)
-        }
 
         relation_mat = np.zeros((self.cwdata.num_obs, self.cwdata.num_defs))
-        relation_mat[range(self.cwdata.num_obs), [indices[alt_def]
+        relation_mat[range(self.cwdata.num_obs), [self.def_indices[alt_def]
                      for alt_def in self.cwdata.alt_defs]] = 1.0
-        relation_mat[range(self.cwdata.num_obs), [indices[ref_def]
+        relation_mat[range(self.cwdata.num_obs), [self.def_indices[ref_def]
                      for ref_def in self.cwdata.ref_defs]] = -1.0
 
         return relation_mat
@@ -125,3 +126,78 @@ class CWModel:
         ).reshape(self.cwdata.num_obs, self.num_var)
 
         return design_mat
+
+    def create_constraint_mat(self):
+        """Create constraint matrix.
+
+        Returns:
+            numpy.ndarray:
+                Return constraints matrix.
+        """
+        if self.order_prior is None:
+            return None
+
+        mat = []
+        for i, d in enumerate(self.order_prior):
+            sub_mat = np.zeros((self.num_var_per_def, self.num_var))
+            sub_mat[:, self.var_indices[d[0]]] = -np.eye( self.num_var_per_def)
+            sub_mat[:, self.var_indices[d[1]]] = np.eye(self.num_var_per_def)
+            mat.append(sub_mat)
+
+        return np.vstack(mat)
+
+    def fit(self, max_iter=100):
+        """Optimize the model parameters.
+        This is a interface to limetr.
+
+        Args:
+            max_iter (int, optional):
+                Maximum number of iterations.
+        """
+        # dimensions for limetr
+        n = self.cwdata.study_sizes
+        k_beta = self.num_var
+        k_gamma = 1
+        Y = self.cwdata.obs
+        S = self.cwdata.obs_se
+        X = self.design_mat
+        Z = np.ones((self.cwdata.num_obs, 1))
+
+        uprior = np.array([[-np.inf]*self.num_var + [0.0],
+                           [np.inf]*self.num_var + [np.inf]])
+        uprior[:, self.var_indices[self.gold_def]] = 0.0
+
+        def F(beta):
+            return X.dot(beta)
+
+        def JF(beta):
+            return X
+
+        if self.constraint_mat is not None:
+            num_constraints = self.constraint_mat.shape[0]
+            C_mat = np.hstack((self.constraint_mat,
+                               np.zeros((num_constraints, 1))))
+            c = np.array([[0.0]*num_constraints,
+                          [np.inf]*num_constraints])
+
+            def C(beta):
+                return C_mat.dot(beta)
+
+            def JC(beta):
+                return C_mat
+
+        else:
+            C = None
+            JC = None
+            c = None
+
+        lt = LimeTr(n, k_beta, k_gamma, Y, F, JF, Z, S=S,
+                    uprior=uprior, C=C, JC=JC, c=c)
+        beta, gamma, _ = lt.fitModel(inner_print_level=5,
+                                     inner_max_iter=max_iter)
+
+        self.beta = {
+            d: beta[self.var_indices[d]]
+            for d in self.cwdata.unique_defs
+        }
+        self.gamma = gamma
